2777ccb739d24260d3255f4c2b6224ff0a354cae
Vlabakhua <volabahua@gmail.com>
2020-09-03 23:07:27 +0300

    # - Переделываю 3 задание

--- 3l_LabakhuaVladimir.playground/Contents.swift
+++ 3l_LabakhuaVladimir.playground/Contents.swift
@@ -11,30 +11,44 @@ enum TrunkCarBrands {
     case kenworth, freightliner, international, caterpillar, mack, autocar
 }
 
+enum ChangeEngineStatus {
+    case furnOnEngine, turnOffEngine
+}
+
+enum ChangeCargoValue {
+    case loadCargo, unloadCargo
+}
+
+enum ChangeWindowsStatus {
+    case openWindows, closeWindows
+}
+
 struct SportCar {
     let carBrand: SportCarBrands
     let yearOfIssue: UInt
     let trunkValue: UInt
     var engineIsOn: Bool
-    mutating func turnOnEngine() {
-        self.engineIsOn = true
-    }
-    mutating func turnOffEngine() {
-        self.engineIsOn = false
-    }
+//    mutating func turnOnEngine() {
+//        self.engineIsOn = true
+//    }
+//    mutating func turnOffEngine() {
+//        self.engineIsOn = false
+//    }
     var windowsIsOpen: Bool
-    mutating func openWindows() {
-        self.windowsIsOpen = true
-    }
-    mutating func closeWindows() {
-        self.windowsIsOpen = false
-    }
+//    mutating func openWindows() {
+//        self.windowsIsOpen = true
+//    }
+//    mutating func closeWindows() {
+//        self.windowsIsOpen = false
+//    }
     var filledTrunkVolume: Int
-    mutating func loadCargo(cargoVolume: Int) {
-        self.filledTrunkVolume += cargoVolume
-    }
-    mutating func unloadCargo(cargoVolume: Int) {
-        self.filledTrunkVolume -= cargoVolume
+    mutating func changeCargo(cargoVolume: Int, mode: ChangeCargoValue) {
+        switch mode  {
+        case .loadCargo:
+            self.filledTrunkVolume += cargoVolume
+        case .unloadCargo:
+            self.filledTrunkVolume -= cargoVolume
+        }
     }
 }
 
@@ -57,11 +71,13 @@ struct TrunkCar {
         self.windowsIsOpen = false
     }
     var filledTrunkVolume: Int
-    mutating func loadCargo(cargoVolume: Int) {
-        self.filledTrunkVolume += cargoVolume
-    }
-    mutating func unloadCargo(cargoVolume: Int) {
-        self.filledTrunkVolume -= cargoVolume
+    mutating func changeCargo(cargoVolume: Int, mode: ChangeCargoValue) {
+        switch mode  {
+        case .loadCargo:
+            self.filledTrunkVolume += cargoVolume
+        case .unloadCargo:
+            self.filledTrunkVolume -= cargoVolume
+        }
     }
     
 }
@@ -76,13 +92,7 @@ var sportcar1 = SportCar(carBrand: .lamborgini, yearOfIssue: 2018, trunkValue: 2
 //Задание 3
 //Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
 
-truck1.openWindows()
-truck1.turnOnEngine()
-truck1.unloadCargo(cargoVolume: 7500)
 
-sportcar1.closeWindows()
-sportcar1.turnOffEngine()
-sportcar1.loadCargo(cargoVolume: 100)
 
 
 //Задание 4
@@ -93,6 +103,13 @@ sportcar1.loadCargo(cargoVolume: 100)
 //Задание 5
 //Инициализировать несколько экземпляров структур. Применить к ним различные действия.
 
+truck1.openWindows()
+truck1.turnOnEngine()
+truck1.unloadCargo(cargoVolume: 7500)
+
+
+//sportcar1.loadCargo(cargoVolume: 100)
+sportcar1.changeCargo(cargoVolume: 20, mode: .loadCargo)
 
 
 //Задание 6

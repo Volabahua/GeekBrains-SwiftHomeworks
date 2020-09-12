a46523f1b310a54341e2c2de80f70040df7e13d5
Vlabakhua <volabahua@gmail.com>
2020-09-13 02:50:35 +0300

    # - дз 5 готово

--- 5l_LabakhuaVladimir.playground/Contents.swift
+++ 5l_LabakhuaVladimir.playground/Contents.swift
@@ -46,25 +46,25 @@ extension Car {
 
 extension Car {
     mutating func loadCargo(cargoVolume: Int) {
-        print("Вы собираетесь положить \(cargoVolume) груза в багажник автомобиля.")
+        print("Вы собираетесь положить \(cargoVolume) груза в кузов.")
         let guardCargo = self.filledTrunkVolume + cargoVolume
         guard  guardCargo < trunkValue else {
-            return print("Вы не можете положить этот груз, в багажнике недостаточно места \nОперация не будет выполнена!")
+            return print("Вы не можете положить этот груз, в кузове недостаточно места \nОперация не будет выполнена!")
         }
         self.filledTrunkVolume += cargoVolume
-        print("После погрузки груза в багажнике стало \(guardCargo) груза")
+        print("После погрузки груза в кузове стало \(guardCargo) груза")
     }
 }
 
 extension Car {
     mutating func unloadCargo(cargoVolume: Int) {
-        print("Вы собираетесь выгрузить \(cargoVolume) груза из багажника автомобиля.")
+        print("Вы собираетесь выгрузить \(cargoVolume) груза из кузова.")
         let guardCargo = self.filledTrunkVolume - cargoVolume
         guard  guardCargo > 0 else {
-           return print("Вы не можете выгрузить этот груз, в багажнике его недостаточно \nОперация не будет выполнена!")
+           return print("Вы не можете выгрузить этот груз, в кузове его недостаточно \nОперация не будет выполнена!")
         }
         self.filledTrunkVolume -= cargoVolume
-        print("После выгрузки груза в багажнике осталось \(guardCargo) груза")
+        print("После выгрузки груза в кузове осталось \(guardCargo) груза")
     }
 }
 
@@ -79,6 +79,7 @@ class SportCar: Car {
     var windowsIsOpen: Bool
     var filledTrunkVolume: Int
     
+    //Отличающиеся свойства
     var maxSpeed: UInt
     var spoiler: Bool
     var turbo: Bool
@@ -104,6 +105,7 @@ class TrunkCar: Car {
     var windowsIsOpen: Bool
     var filledTrunkVolume: Int
     
+    //Отличающиеся свойства
     var rangeOnOneTank: UInt
     var hydraulicLift: Bool
     var refrigerator: Bool
@@ -126,24 +128,33 @@ class TrunkCar: Car {
 
 extension SportCar: CustomStringConvertible {
     var description: String {
-        return "Спортивный автомобиль \(yearOfIssue) года выпуска \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen)"
+        return "Спортивный автомобиль \(yearOfIssue) года выпуска \nМаксимальная скорость - \(maxSpeed) \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen)\nИмеет спойлер - \(spoiler) \nИмеет турбо - \(turbo) "
     }
 }
 
 extension TrunkCar: CustomStringConvertible {
     var description: String {
-        return "Грузовой автомобиль \(yearOfIssue) года выпуска \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen)"
+        return "Грузовой автомобиль \(yearOfIssue) года выпуска \nДальность поездки на одном баке - \(rangeOnOneTank) \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen) \nИмеет гидравлический лифт - \(hydraulicLift) \nИмеет холодильную установку - \(refrigerator)"
     }
 }
 
 //Задание 5
 //Создать несколько объектов каждого класса. Применить к ним различные действия.
 
-let honda = SportCar(color: .red, yearOfIssue: 2020, trunkValue: 300, engineIsOn: false, windowsIsOpen: false, filledTrunkVolume: 0)
+var honda = SportCar(color: .red, yearOfIssue: 2020, trunkValue: 300, engineIsOn: false, windowsIsOpen: true, filledTrunkVolume: 15, maxSpeed: 240, spoiler: true, turbo: false)
 
-let man = TrunkCar
+var man = TrunkCar(color: .blue, yearOfIssue: 1995, trunkValue: 50000, engineIsOn: true, windowsIsOpen: false, filledTrunkVolume: 10000, rangeOnOneTank: 900, hydraulicLift: true, refrigerator: false)
+
+honda.loadCargo(cargoVolume: 30)
+honda.closeWindow()
+honda.startEngine()
+
+man.unloadCargo(cargoVolume: 3500)
+man.openWindow()
+man.stopEngine()
 
 //Задание 6
 //Вывести сами объекты в консоль.
 
 print(honda)
+print(man)

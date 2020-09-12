ba35b06447f5a0a677bba13036a798f4d9e6139f
Vlabakhua <volabahua@gmail.com>
2020-09-13 02:36:47 +0300

    # - почти закончил

--- 5l_LabakhuaVladimir.playground/Contents.swift
+++ 5l_LabakhuaVladimir.playground/Contents.swift
@@ -3,15 +3,6 @@ import UIKit
 //Задание 1
 //Создать протокол «Car» и описать свойства, общие для автомобилей, а также метод действия.
 
-enum ChangeCargoValue {
-    case loadCargo, unloadCargo
-}
-
-enum CarActions{
-    case openWindows, closeWindows, furnOnEngine, turnOffEngine
-}
-
-
 protocol Car {
     var color: UIColor { get set }
     var yearOfIssue: UInt { get set }
@@ -80,19 +71,79 @@ extension Car {
 //Задание 3
 //Создать два класса, имплементирующих протокол «Car» - trunkCar и sportСar. Описать в них свойства, отличающиеся для спортивного автомобиля и цистерны.
 
+class SportCar: Car {
+    var color: UIColor
+    var yearOfIssue: UInt
+    var trunkValue: UInt
+    var engineIsOn: Bool
+    var windowsIsOpen: Bool
+    var filledTrunkVolume: Int
+    
+    var maxSpeed: UInt
+    var spoiler: Bool
+    var turbo: Bool
+    
+    init(color: UIColor, yearOfIssue: UInt, trunkValue: UInt, engineIsOn: Bool, windowsIsOpen: Bool, filledTrunkVolume: Int, maxSpeed: UInt, spoiler: Bool, turbo: Bool) {
+        self.color = color
+        self.yearOfIssue = yearOfIssue
+        self.trunkValue = trunkValue
+        self.engineIsOn = engineIsOn
+        self.windowsIsOpen = windowsIsOpen
+        self.filledTrunkVolume = filledTrunkVolume
+        self.maxSpeed = maxSpeed
+        self.spoiler = spoiler
+        self.turbo = turbo
+    }
+}
 
+class TrunkCar: Car {
+    var color: UIColor
+    var yearOfIssue: UInt
+    var trunkValue: UInt
+    var engineIsOn: Bool
+    var windowsIsOpen: Bool
+    var filledTrunkVolume: Int
+    
+    var rangeOnOneTank: UInt
+    var hydraulicLift: Bool
+    var refrigerator: Bool
+    
+    init(color: UIColor, yearOfIssue: UInt, trunkValue: UInt, engineIsOn: Bool, windowsIsOpen: Bool, filledTrunkVolume: Int, rangeOnOneTank: UInt, hydraulicLift: Bool, refrigerator: Bool) {
+        self.color = color
+        self.yearOfIssue = yearOfIssue
+        self.trunkValue = trunkValue
+        self.engineIsOn = engineIsOn
+        self.windowsIsOpen = windowsIsOpen
+        self.filledTrunkVolume = filledTrunkVolume
+        self.rangeOnOneTank = rangeOnOneTank
+        self.hydraulicLift = hydraulicLift
+        self.refrigerator = refrigerator
+    }
+}
 
 //Задание 4
 //Для каждого класса написать расширение, имплементирующее протокол CustomStringConvertible.
 
+extension SportCar: CustomStringConvertible {
+    var description: String {
+        return "Спортивный автомобиль \(yearOfIssue) года выпуска \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen)"
+    }
+}
 
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "Грузовой автомобиль \(yearOfIssue) года выпуска \nМаксимальной вместительностью \(trunkValue) груза \nДвигатель запущен - \(engineIsOn) \nОкна открыты - \(windowsIsOpen)"
+    }
+}
 
 //Задание 5
 //Создать несколько объектов каждого класса. Применить к ним различные действия.
 
+let honda = SportCar(color: .red, yearOfIssue: 2020, trunkValue: 300, engineIsOn: false, windowsIsOpen: false, filledTrunkVolume: 0)
 
+let man = TrunkCar
 
 //Задание 6
 //Вывести сами объекты в консоль.
 
-
+print(honda)
